551e4ed51907877edeba8a895594bfa5fef74826
unknown <alexkana@csd.auth.gr>
2022-07-17 23:53:30 +0300

    Movie upd

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => { res.header('Access-Control-Allow-Origin', '*'); next(); });
+app.use((req, res, next) => { 
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+ next(); 
+});
 
 // Import movie routes
 const movieRoutes = require("./routes/movieRoutes");
